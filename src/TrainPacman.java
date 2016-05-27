aa3893238535127dd00ab8ccd8a1fa38dcddeb9a
= <terrencezli@hotmail.com>
2016-05-26 22:13:27 -0700

    trainpacman object

--- src/TrainPacman.java
+++ src/TrainPacman.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -7,27 +8,80 @@ import java.util.Map;
  * Created by terrence on 5/21/16.
  */
 public class TrainPacman {
+    File trainingFile;
+    int loopSize;
+    int cacheSize;
+    List<Integer> trainingData;
+    Map<Integer, Integer> memDistance;
+    Map<Integer, String> cacheChoice;
+
+    public TrainPacman() {
+    }
+
+    public TrainPacman(File trainingFile, int loopSize, int cacheSize) {
+        this.trainingFile = trainingFile;
+        this.loopSize = loopSize;
+        this.cacheSize = cacheSize;
+
+        this.trainingData = new ArrayList<>();
+        this.memDistance = new HashMap<>();
+        this.cacheChoice = new HashMap<>();
+    }
+
+    public File getTrainingFile() {
+        return trainingFile;
+    }
+
+    public int getLoopSize() {
+        return loopSize;
+    }
+
+    public int getCacheSize() {
+        return cacheSize;
+    }
+
+    public List<Integer> getTrainingData() {
+        return trainingData;
+    }
+
+    public Map<Integer, Integer> getMemDistance() {
+        return memDistance;
+    }
+
+    public Map<Integer, String> getCacheChoice() {
+        return cacheChoice;
+    }
+
+    public void setTrainingFile(File trainingFile) {
+        this.trainingFile = trainingFile;
+    }
+
+    public void setLoopSize(int loopSize) {
+        this.loopSize = loopSize;
+    }
+
+    public void setCacheSize(int cacheSize) {
+        this.cacheSize = cacheSize;
+    }
+
+    public void setTrainingData(List<Integer> trainingData) {
+        this.trainingData = trainingData;
+    }
+
+    public void setMemDistance(Map<Integer, Integer> memDistance) {
+        this.memDistance = memDistance;
+    }
+
+    public void setCacheChoice(Map<Integer, String> cacheChoice) {
+        this.cacheChoice = cacheChoice;
+    }
+
     /**
      *
-     * @param args file cachesize loopsize
+     * train the pacman data
+     * training data will be two loops
      */
-    public static void main(String args[]) {
-        //int cacheSize = Integer.parseInt(args[1]);
-        int cacheSize = 3;
-        int loopSize = 4;
-        List<Integer> trainingData = new ArrayList<>();
-        Map<Integer, Integer> memDistance = new HashMap<>();
-        Map<Integer, String> cacheChoice = new HashMap<>();
-
-        trainingData.add(0);
-        trainingData.add(100);
-        trainingData.add(200);
-        trainingData.add(300);
-        trainingData.add(14);
-        trainingData.add(112);
-        trainingData.add(215);
-        trainingData.add(313);
-
+    public void train() {
         for (int i = 0; i < trainingData.size()/2; i++) {
             Integer address1 = trainingData.get(i);
 
