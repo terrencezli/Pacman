963eee370d718973e0ac786642fcb868a84709ad
Timothy Chu <tchu01@calpoly.edu>
2016-06-01 17:17:36 -0700

    Adding Abstract Cache, Cache Interface, Lru, and Simulator

---
+++ src/AbstractCache.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Timothy Chu on 6/1/2016.
+ */
+public class AbstractCache {
+}
---
+++ src/Simulator.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Timothy Chu on 6/1/2016.
+ */
+public class Simulator {
+    public static void main(String args[]) {
+        Lru test = new Lru(3, 1);
+        test.put(1, 1);
+        test.put(2, 2);
+        test.put(3, 3);
+        test.get(1);
+        test.put(4, 4);
+
+
+
+    }
+
+}
